3ff132a372f9212901c4f439cba4ba3b29a298d1
Marc AWAD <marc.awad@supdevinci-edu.fr>
2026-01-13 11:45:16 +0100

    intial commit

---
+++ src/main/java/com/example/gestion_the/GestionTheApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestion_the;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionTheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionTheApplication.class, args);
+	}
+
+}
